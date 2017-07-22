f29a5deb9c98346189948e441c1da141365b609d
Greg Chamberlain <gregchamberlain94@gmail.com>
2017-07-22 18:01:50 -0400

    Added olwisconse.com to CORS

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const schema = makeExecutableSchema({ typeDefs, resolvers });
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://olwisconse.surge.sh'],
+  origin: ['http://localhost:3000', 'https://olwisconse.surge.sh', 'http://www.olwisconse.com'],
   credentials: true
 }));
 
